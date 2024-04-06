a8bd8b3f46c77a1dbb71352f07789a234c57851c
Naoki <yanshangxi4@gmail.com>
2024-04-06 11:40:56 +0900

    変更しました

--- mobile_App/backend/files_judgement.py
+++ mobile_App/backend/files_judgement.py
@@ -1,27 +1,24 @@
 import pandas as pd
 
-df= pd.DataFrame(raw_data_excel)
-tateretu= df.index
-yokoretu= df.columns
-aggfunc= ""
-
-
-
-
-
 
 def judgement(file_name):
     for filename in file_name.values():
-    
         file_extension = filename.split(".")[-1]
         
-        
         if file_extension== 'xlsx':
-            raw_data_excel= pd.read_excel(filename)
+            raw_data= pd.read_excel(filename)
             
+
         elif file_extension== 'csv':
-            raw_data_csv= pd.read_csv(filename)
+            raw_data= pd.read_csv(filename)
+    
+    
+    return raw_data
 
-        print("File extension:", file_extension)
-        return file_extension
 
+"""
+df= pd.DataFrame(raw_data_excel)
+tateretu= df.index
+yokoretu= df.columns
+aggfunc= "mean"
+"""
